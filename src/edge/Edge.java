f7ca197a2db55937a9a4df6c5a6ff2423df21c32
ArdaAsan1448 <asanarda16@gmail.com>
2026-04-27 23:43:53 +0300

    fix: add missing Edge.java file

--- src/edge/Edge.java
+++ src/edge/Edge.java
@@ -1,20 +1,31 @@
-package edge;
+package edge; 
 
-import node.Node;
+import node.Node; 
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
 
 public class Edge {
-
     private Node source;
-    private Node target;
-    private Map<String, Objects> properties;
+    private Node destination;
+    private EdgeType type;
+    private boolean directed;
 
-    public Edge(Node source, Node target) {
+    public Edge(Node source, Node destination, EdgeType type, boolean directed) {
         this.source = source;
-        this.target = target;
-        this.properties = new HashMap<>();
+        this.destination = destination;
+        this.type = type;
+        this.directed = directed;
     }
+
+    // Getter ve Setter metotları 
+    public Node getSource() { return source; } //Private olan değişkenleri dışarıdan okumamıza izin verir.
+    public void setSource(Node source) { this.source = source; } // Private olan değişkenleri güvenli bir şekilde değiştirmemize izin verir.
+
+    public Node getDestination() { return destination; }
+    public void setDestination(Node destination) { this.destination = destination; }
+
+    public EdgeType getType() { return type; }
+    public void setType(EdgeType type) { this.type = type; }
+
+    public boolean isDirected() { return directed; }
+    public void setDirected(boolean directed) { this.directed = directed; }
 }
