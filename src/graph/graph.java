c21e2fe2f86ad3c64cca3c8d62e8acaef0ba3aa3
Fatih Soyer <soyerf18@gmail.com>
2026-04-28 17:20:51 +0300

    deneme amaçlıdır.

--- src/graph/graph.java
+++ src/graph/graph.java
@@ -1,7 +1,7 @@
 package graph;
 
 public class graph {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         System.out.println("gencel bey kodu yazınca startı vereceğim");
     }
 }
